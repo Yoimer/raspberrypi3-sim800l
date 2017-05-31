a6afe117df71354e779c0afb485e36c15542942f
Yoimer <ydrf19@gmail.com>
2017-05-30 21:22:52 -0400

    Added expected_answer string on sim800_responde function

--- sendATcommand.py
+++ sendATcommand.py
@@ -4,55 +4,59 @@ import time
 
 phone = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
 
-def sim800_responde():
+def sim800_responde(expected_answer):
     while True:
-        response = phone.readline()    
+        response = phone.readline()
         print  response
-        if "OK" in response:
+        if expected_answer in response:
             break
-     
 
-def web(): 
-  
-  
+
+def web():
+
+
     phone.write('AT+CFUN=0\r')
-    sim800_responde()
+    sim800_responde(expected_answer = "OK")
+
 
     phone.write('AT+CFUN=1\r')
-    sim800_responde()
+    sim800_responde(expected_answer = "Call Ready")
 
-    phone.write('AT+SAPBR=3,1,"APN","internet.movistar.ve"\r')
-    sim800_responde()
- 
-    phone.write('AT+SAPBR=1,1\r')
-    sim800_responde()
-  
+    # phone.write('AT+CFUN=1\r')
+    # sim800_responde()
 
-    phone.write('AT+SAPBR=2,1\r')
-    sim800_responde()
+    # phone.write('AT+SAPBR=3,1,"APN","internet.movistar.ve"\r')
+    # sim800_responde()
+
+    # phone.write('AT+SAPBR=1,1\r')
+    # sim800_responde()
 
 
-    phone.write('AT+HTTPINIT\r')
-    sim800_responde()
+    # phone.write('AT+SAPBR=2,1\r')
+    # sim800_responde()
 
-    phone.write('AT+HTTPPARA="CID",1\r')
-    sim800_responde()
 
-    phone.write('AT+HTTPPARA="URL","http://sandro.awardspace.info/php/hola.php?Tu_nombre=Renzo"\r')
-    sim800_responde()
-  
-    phone.write('AT+HTTPACTION=0\r')
-    sim800_responde()
-    time.sleep(3)
+    # phone.write('AT+HTTPINIT\r')
+    # sim800_responde()
 
-    phone.write('AT+HTTPREAD\r')
-    sim800_responde()
+    # phone.write('AT+HTTPPARA="CID",1\r')
+    # sim800_responde()
 
-    phone.write('AT+HTTPTERM\r')
-    sim800_responde()
+    # phone.write('AT+HTTPPARA="URL","http://sandro.awardspace.info/php/hola.php?Tu_nombre=Renzo"\r')
+    # sim800_responde()
 
-    phone.write('AT+SAPBR=0,1\r')   
-    sim800_responde()
-  
+    # phone.write('AT+HTTPACTION=0\r')
+    # sim800_responde()
+    # time.sleep(3)
+
+    # phone.write('AT+HTTPREAD\r')
+    # sim800_responde()
+
+    # phone.write('AT+HTTPTERM\r')
+    # sim800_responde()
+
+    # phone.write('AT+SAPBR=0,1\r')
+    # sim800_responde()
+
 
 web()
